c8f6af8b65c0d2a2f83167510c89f551ae65eb2e
aminata <mamy.ndiaye98@gmail.com>
2017-01-12 14:51:52 +0100

    Modofication galerie.php
    Fusion bibliothèque.html et galerie.php

--- galerie.php
+++ galerie.php
@@ -1,14 +1,47 @@
 <?php
-require ('connect.php');
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
+require('connect.php');
 
-$gal = $dbh->prepare('SELECT COUNT(:id) FROM image');
-$gal->execute([
-    ':id' => id,
-]);
-$page = $gal/5;
+if (isset($_POST['resultats'])) {
+    $_SESSION['nb_max'] = intval($_POST['resultats']);
+    header('Location:galerie.php?page=1');
+}
 
+function get_page_images($page, $nb_resultats)
+{
+    global $dbh;
+    $fin = intval($page) * intval($nb_resultats);
+    $debut = $fin - intval($nb_resultats) + 1;
+
+    $req = $dbh->prepare('SELECT lien FROM image LIMIT :debut,:fin');
+    $req->bindParam(':debut', $debut, PDO::PARAM_INT);
+    $req->bindParam(':fin', $fin, PDO::PARAM_INT);
+    $req->execute();
+    $images = $req->fetchAll();
+    var_dump($debut, ' ', $fin);
+    global $dbh;
+    $req = $dbh->prepare('SELECT COUNT(*) as count FROM image');
+    $req->execute();
+    $count = $req->fetch()['count'];
+
+    $max = intval($count) / $_SESSION['nb_max'];
+    var_dump($max);
+    if ($max<=$debut) {
+        echo 'Aucune image disponible';
+    }
+    return $images;
+}
+
+if (!empty($_GET['page'])) {
+    $page = intval($_GET['page']);
+    $precedent = $page - 1;
+    $suivant = $page + 1;
+} else {
+    header('Location: galerie.php?page=1');
+}
+
+if ($page <= 0) {
+    header('Location: galerie.php?page=1');
+}
 ?>
 <!doctype html>
 <!DOCTYPE html>
@@ -16,28 +49,49 @@ $page = $gal/5;
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./css/bibliotheque.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
 <body>
 <h1>Bibliothèque D'Images</h1>
-<div id="photo1">
-</div>
-<div id="photo2">
-</div>
-<div id="photo3">
-</div>
-<div id="photo4">
-	
-</div>
-<div id="photo5"></div>
-<div id=photo6></div>
-<div id="photo7"></div>
-<div id="photo8"></div>
-<div id="photo9"></div>
-<br><form>
-    <a href="page2.html"><input type="button" value="suivant" onclick="" class="bvalid" ></a>
+
+<p>Nombre d'image par page :</p>
+<form action="" method="post">
+    <?php var_dump($_SESSION); ?>
+    <select class="form-control" name="resultats">
+        <option value="10" <?= ($_SESSION['nb_max'] == '10') ? 'selected' : '' ?>>10</option>
+        <option value="15" <?= ($_SESSION['nb_max'] == '15') ? 'selected' : '' ?>>15</option>
+        <option value="20" <?= ($_SESSION['nb_max'] == '20') ? 'selected' : '' ?>>20</option>
+        <option value="25" <?= ($_SESSION['nb_max'] == '25') ? 'selected' : '' ?>>25</option>
+        <option value="30" <?= ($_SESSION['nb_max'] == '30') ? 'selected' : '' ?>>30</option>
+    </select>
+    <input type="submit" name="Envoyer">
+</form>
+<?php
+$images = get_page_images($page, $_SESSION['nb_max']);
+foreach ($images as $cle => $image) {
+    $images[$cle]['name'] = htmlentities($images['name']);
+    ?>
+    <h2><?= $image['name'] ?></h2>
+    <img src="<?= $image['link'] ?>" alt="" id="photo1">
+<?php } ?>
+
+<br>
+<form>
+    <a href="?page=<?php if (isset($precedent)) echo $precedent; ?>"><input type="button" value="precedent"
+                                                                            class="bvalid"></a>
+    <a href="?page=<?php if (isset($suivant)) echo $suivant; ?>"><input type="button" value="suivant"
+                                                                        class="bvalid"></a>
 </form>
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 </body>
 </html>
+
+
