b956c26e888a18b2dad563048bd72a71b0705f28
RePloZ <herve.kay@ynov.com>
2017-01-18 11:08:14 +0100

    base de donnée sarah
    repair broken code

--- sql/user.sql
+++ sql/user.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 15 Janvier 2017 à 22:44
+-- Généré le :  Mar 17 Janvier 2017 à 12:38
 -- Version du serveur :  5.7.14
--- Version de PHP :  7.0.10
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,25 +29,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
+  `prénom` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `adresse` varchar(255) NOT NULL,
-  `ville` varchar(255) NOT NULL,
-  `pays` varchar(255) NOT NULL,
-  `anniversaire` date NOT NULL,
   `sexe` varchar(255) NOT NULL,
-  `tel` int(10) NOT NULL,
-  `fixe` int(10) NOT NULL,
-  `codepostal` int(5) NOT NULL
+  `tel` int(11) NOT NULL,
+  `anniversaire` date NOT NULL,
+  `codepostal` int(5) NOT NULL,
+  `pays` varchar(255) NOT NULL,
+  `ville` varchar(255) NOT NULL,
+  `fixe` int(11) NOT NULL,
+  `adresse` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `prenom`, `email`, `password`, `adresse`, `ville`, `pays`, `anniversaire`, `sexe`, `tel`, `fixe`, `codepostal`) VALUES
-(1, 'kay', 'herve', 'ha@fr.de', '28JKG4P.L00TQ', '2,lklkl', 'sarcekkes', 'france', '2014-11-30', 'Masculin', 606060606, 606060606, 95784);
 
 --
 -- Index pour les tables exportées
@@ -67,7 +60,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
